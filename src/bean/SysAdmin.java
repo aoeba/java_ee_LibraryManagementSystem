a82ecaa8f0c4097efd3af3bfd53e09694bde601a
1114951496 <1114951496@qq.com>
2016-11-21 14:54:13 +0800

    yyh11.21

---
+++ src/bean/SysAdmin.java
@@ -0,0 +1,41 @@
+package bean;
+
+public class SysAdmin {
+
+	private String manager_name;
+	private int manager_id;
+	private String password;
+	private String phone_number;
+	private int id_number;
+	public String getManager_name() {
+		return manager_name;
+	}
+	public void setManager_name(String manager_name) {
+		this.manager_name = manager_name;
+	}
+	public int getManager_id() {
+		return manager_id;
+	}
+	public void setManager_id(int manager_id) {
+		this.manager_id = manager_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public int getId_number() {
+		return id_number;
+	}
+	public void setId_number(int id_number) {
+		this.id_number = id_number;
+	}
+	
+}
